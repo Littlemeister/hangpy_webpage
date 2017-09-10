def4ed12300ab0975fd4ec1d408a42e0a7ff045a
Littlemeister <Littlemeister_@hotmail.com>
2017-09-10 15:40:11 +0200

    header

--- components/header.php
+++ components/header.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="header_style.css">
 
 <header class="header_wrapper">
-  <img src="hangpylogo.png" alt="Hangpy logo">
+  <img src="../pic/hangpy_logo.png" alt="Hangpy logo">
   <button id="header_register" type="button" name="button">Register</button>
   <button id="header_login" type="button" name="button">Login</button>
 </header>
--- index.php
+++ index.php
@@ -19,20 +19,21 @@ require_once 'lib/backend_com.php';
 
 <html lang="sv">
 
-<head>
-  <meta charset="utf-8">
+  <head>
+    <meta charset="utf-8">
 
-  <title>Hangpy</title>
+    <title>Hangpy</title>
 
-  <meta name="description" content="Fun events">                        <!-- Specifies a description of the page. Search engines can pick up this description to show with the results of searches. -->
-  <meta name="keywords" content="fun,event,events,spontant,hitta på, vad händer,">                    <!-- Specifies a comma-separated list of keywords - relevant to the page (Informs search engines what the page is about). -->
-  <meta name="application-name" content="Hangpy">                       <!-- Specifies the name of the Web application that the page represents -->
+    <meta name="description" content="Fun events">                        <!-- Specifies a description of the page. Search engines can pick up this description to show with the results of searches. -->
+    <meta name="keywords" content="fun,event,events,spontant,hitta på, vad händer,">                    <!-- Specifies a comma-separated list of keywords - relevant to the page (Informs search engines what the page is about). -->
+    <meta name="application-name" content="Hangpy">                       <!-- Specifies the name of the Web application that the page represents -->
 
-  <!-- Responsive design thingy -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">  <!-- Specifies the scaling of the site when in mobile resulution so text and pictures doesnt get to small -->
+    <!-- Responsive design thingy -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">  <!-- Specifies the scaling of the site when in mobile resulution so text and pictures doesnt get to small -->
 
-  <!-- Link stylecheat -->
-  <link rel="stylesheet" href="style.css">
+    <!-- Link stylecheat -->
+    <link rel="stylesheet" href="style.css">          <!-- Global style -->
+    <link rel="stylesheet" href="header_style.css">   <!-- Style for header/global menu -->
 
   </head>
 
