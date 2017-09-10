9ad6fdf641fc0e8baf5670a1359332c6b49ef6ef
Littlemeister <Littlemeister_@hotmail.com>
2017-09-10 13:24:14 +0200

    Andreas Poulsen

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 
 /**
 *   HANGPY WEB START POINT
+*   Version: 0.01
+*   Made by: Andreas Poulsen and Johan Svensson
+*   Date 2017-09 to 2017-09
 */
 
 //  User functions and data
@@ -14,19 +17,38 @@ require_once 'lib/backend_com.php';
 
 <!DOCTYPE html>
 
-<html>
-    
-<body>
-    Events:
-    <div id="events">
-    </div>
-    
-    <?php
-    require 'scripts.php';
-    ?>
-    <script src="scripts/events.js"></script>
-    
-    <script>Events.fetch();</script>
-</body>
+<html lang="sv">
+
+<head>
+  <meta charset="utf-8"> <!-- What charset character set to be used -->
+
+  <title>Hangpy</title> <!-- The pages title -->
+
+  <meta name="description" content="Fun events">                  <!-- Specifies a description of the page. Search engines can pick up this description to show with the results of searches. -->
+  <meta name="keywords" content="fun,event,events,spontant,hitta på, vad händer,">                    <!-- Specifies a comma-separated list of keywords - relevant to the page (Informs search engines what the page is about). -->
+  <meta name="application-name" content="Hangpy">                       <!-- Specifies the name of the Web application that the page represents -->
+
+  <!-- Responsive design thingy -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">  <!-- Specifies the scaling of the site when in mobile resulution so text and pictures doesnt get to small -->
+
+  <!-- Link stylecheat -->
+  <link rel="stylesheet" href="style.css"> <!-- Link stylecheat -->
+
+  <!-- javascript -->
+  <script src="scripts/main.js"></script>
+  <script src="scripts/events.js"></script>
+  <script>Events.fetch();</script>
+
+  </head>
+
+  <body>
+      Events:
+      <div id="events">
+      </div>
+
+      <?php
+        require 'scripts.php';
+      ?>
+  </body>
 
 </html>
