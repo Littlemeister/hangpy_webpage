68313841b5000c731148d94843e543fed7a5f03b
Littlemeister <Littlemeister_@hotmail.com>
2017-09-10 19:18:18 +0200

    footer v.0.5

--- components/footer.php
+++ components/footer.php
@@ -3,20 +3,16 @@
   <div class="footer_inside_content_wrapper">
     <div class="footer_content_1">
       <h3>About</h3>
-      <p> An webbservice to connect people, hang out and be happy.
-        Create, find and join events in your nabourhood in a easy, fast and fun way!
-        Hangpy exist as both webbpage, app for IOS and android.
-      </p>
+      <p>Create, find and join events in your nabourhood in a easy, fast and fun way!
+        Hangpy exist as both webbpage, app for IOS and android.</p>
     </div>
     <div class="footer_content_2">
       <h3>Contact</h3>
-      <p>Hangpy - hang out be happy. An webbservice to connect people.
-        Create, find and join events in your nabourhood in a easy, fast and fun way!
-        Hangpy exsist as both webbpage, app for IOS and android.
-      </p>
+      <button>email: </button>
     </div>
     <div class="footer_content_3">
       <h3>Devolopers</h3>
+      <p></p>
     </div>
   </div>
 
